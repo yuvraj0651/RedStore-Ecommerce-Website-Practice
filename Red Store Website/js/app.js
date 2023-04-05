4de8d5c66a478c2f0cac679725cc5c9967600909
Yuvraj-Ganshi <85454673+yuvraj0651@users.noreply.github.com>
2023-04-05 11:02:18 +0530

    Add files via upload

--- Red Store Website/js/app.js
+++ Red Store Website/js/app.js
@@ -31,3 +31,25 @@ function login()
     LoginForm.style.transform = "translateX(300px)";
     Indicator.style.transform = "translateX(0px)";
 }
+
+//js for product gallary
+
+var ProductImg = document.getElementById("productImg");
+var SmallImg = document.getElementsByClassName("smallImg");
+
+SmallImg[0].onclick = function()
+{
+    ProductImg.src = SmallImg[0].src;
+}
+SmallImg[1].onclick = function()
+{
+    ProductImg.src = SmallImg[1].src;
+}
+SmallImg[2].onclick = function()
+{
+    ProductImg.src = SmallImg[2].src;
+}
+SmallImg[3].onclick = function()
+{
+    ProductImg.src = SmallImg[3].src;
+}
